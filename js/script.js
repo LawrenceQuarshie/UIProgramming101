cd146e21c9139b6f53dd5838cac9c7f1f67bc584
Lawrence <lquarshie29@gmail.com>
2024-05-18 11:06:01 -0400

    Updated the nav bar by including buttons for mobile.

--- js/script.js
+++ js/script.js
@@ -4,5 +4,30 @@ function populateInput() {
 }
 
 function displayProducts() {
-    document.getElementById("ul-id2").style.display = "block";
+    let y = document.getElementById("ul-id2");
+
+    if(y.style.display == "block") {
+        y.style.display = "none";
+    } else {
+        y.style.display = "block";
+    }
+}
+
+function showSearchResults() {
+    document.getElementById("ul-id3").style.display = "block";
+
+    let a = document.getElementById("input-id1").value;
+    console.log(a);
+    // console.log(b)
+}
+
+function domEvents(event) {
+    let c = event.target;
+    console.log(c)
+
+    // if(c != "INPUT") {
+    //     document.getElementById("ul-id3").style.display = "none";
+    // } else {
+    //     document.getElementById("ul-id3").style.display = "block";
+    // }
 }
